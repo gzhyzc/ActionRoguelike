ccc1caa2e97d63b187651b8e276030c2369fb3f8
yangyan <1051117332@qq.com>
2022-07-24 22:40:54 +0800

    fire

---
+++ Source/ActionRoguelike/Public/SExplosiveBarrel.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PhysicsEngine/RadialForceComponent.h"
+#include "SExplosiveBarrel.generated.h"
+
+UCLASS()
+class ACTIONROGUELIKE_API ASExplosiveBarrel : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASExplosiveBarrel();
+
+protected:
+
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly)
+	UStaticMeshComponent* MeshComp;
+
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly)
+	URadialForceComponent* ForceComp;
+
+	virtual void PostInitializeComponents() override;
+	
+	UFUNCTION()
+	void OnActorHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
